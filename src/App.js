c6cd94faab3466831fd4089cb65a4cb3ef75dd6f
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-09 22:54:38 +0600

    Created All the routes(Except error)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+import Home from './routes/home/home_component';
 import './App.css';
+import About from './routes/about/about_component';
+import Product from './routes/product/product_component';
+import Products from './routes/products/products_component';
+import Contact from './routes/contact/contact_component';
+import Cart from './routes/cart/cart_component';
 
 function App() {
   return (
-    <div>Hello</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/product' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+
+      </Routes>
+    
+    </BrowserRouter>
   );
 }
 
