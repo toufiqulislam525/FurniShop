6e6894ffc02528040b8ad28c0ff03e3e928fa5cb
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-10 03:14:01 +0600

    Basic Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,48 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from '../assets/logo.svg'
 import { FaBars } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import { links } from '../utils/constants'
 import CartButtons from './CartButtons'
 import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  return <h4>navbar</h4>
+  return (
+    <NavContainer>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <img src= {logo} alt='comfy slot' />
+          </Link>
+          <button type='button' className='nav-toggle'>
+          <FaBars />
+        </button>
+        </div>
+        <ul className='nav-links'>
+          {links.map((link)=>{
+            const {id,text,url} = link;
+            return(
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+              )
+            })}
+        </ul>
+        <CartButtons/>
+          
+        
+      </div>
+      
+
+    </NavContainer>
+    
+
+  )
 }
 
 const NavContainer = styled.nav`
-  height: 5rem;
+  height: 8rem;
   display: flex;
   align-items: center;
   justify-content: center;
