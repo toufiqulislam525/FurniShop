145e3cc0274e9942271ce153c7c30b09c4726e30
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-10 14:21:44 +0600

    Fetching all products and calc featured products

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,7 +15,11 @@ import {
 
 const initialState = {
 
-  isSidebarOpen : false
+  isSidebarOpen : false,
+  products_loading : false,
+  products_error : false,
+  products : [],
+  featured_products : [],
 }
 
 const ProductsContext = React.createContext()
@@ -34,6 +38,23 @@ export const ProductsProvider = ({ children }) => {
     dispatch( {type : SIDEBAR_CLOSE});
 
   }
+
+  const fetchProducts = async(url) =>{
+    dispatch({type : GET_PRODUCTS_BEGIN});
+    try{
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({type:GET_PRODUCTS_SUCCESS,payload:products});
+    }catch(error){
+      dispatch({type : GET_PRODUCTS_ERROR});
+    }
+    
+    
+  }
+
+  useEffect(()=>{
+    fetchProducts(url)
+  },[])
 
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -12,9 +13,12 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-   <ProductsProvider>
-        <App />
-    </ProductsProvider> 
+    <StrictMode>
+        <ProductsProvider>
+            <App />
+        </ProductsProvider> 
+    </StrictMode>
+   
 
 
 );
