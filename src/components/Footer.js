279d75ca201f6a161ec41a787426c2ded548cb6d
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-12 02:38:03 +0600

    Added Checkout Page Functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <Wrapper>
       <h5>
         &copy; {new Date().getFullYear()}
-        <span> ComfySloth</span>
+        <span> FurniShop</span>
       </h5>
       <h5>All rights Reserved</h5>
 
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
-// extra imports
-import { useCartContext } from '../context/cart_context'
+import { PageHero} from '../components'
 import { Link } from 'react-router-dom'
+import { useUserContext } from '../context/user_context'
+import { useCartContext } from '../context/cart_context'
+// extra imports
+// import { Link } from 'react-router-dom'
 
 const CheckoutPage = () => {
+  const {myUser} = useUserContext();
+  let n = ''
+  if(myUser){
+    const {name,nickname} = myUser;
+    if(name){
+      n = name;
+    }
+    if(nickname){
+      n = nickname;
+    }
+  }
+  const {clearCart} = useCartContext();
+  
+  
 
   return (
     <main>
       <PageHero title='checkout' />
       <Wrapper className='page'>
-        <h1>Checkout Here </h1>
+        <div className='section-center section'>
+          <h2>Thanks For Shopping {`${myUser ? n : ''}`}</h2>
+          <hr/>
+          <br/>
+          
+          <Link to='/products' className='btn' onClick={clearCart}>
+            Clear Cart ? 
+          </Link>
+        </div>
+        
       </Wrapper>
     </main>
   )
