e5a915d13909adf445ea2d2dfef491922b39fd72
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-12 04:45:49 +0600

    routing for 404 page

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       <Sidebar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/FurniShop' element={<Home />}/>
+        <Route path='/FurniShop/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/cart' element={<Cart />}/>
         <Route path='/products/' element={<Products />}/>
