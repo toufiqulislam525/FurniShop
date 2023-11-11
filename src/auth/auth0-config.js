4a74a98010b8d7850ac1ca16feffc697780c4557
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-12 04:58:20 +0600

    t

--- src/auth/auth0-config.js
+++ src/auth/auth0-config.js
@@ -1,7 +1,7 @@
 const domain = "dev-p1gix4nnojy5oq4q.jp.auth0.com";
 const clientId = "l0GDFkHEBksgjSTwrwFforHPEw1h4PKz";
 const cacheLocation='localstorage'
-const redirectUri = 'https://toufiqulislam525.github.io/FurniShop'
+const redirectUri = 'https://toufiqulislam525.github.io'
 
 export const authConfig = {
   domain,
